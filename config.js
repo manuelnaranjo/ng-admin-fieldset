48850f7f39ff4f82fb91d7bbf967d7a9337547cc
Manuel Francisco Naranjo <naranjo.manuel@gmail.com>
2016-01-07 01:07:13 -0300

    [initial] Adding initial commits
    This code has been residing in my fieldset branch for quite sometime,
    I'm refactoring it so that it can be reused without having to create a
    separate branch from ng-admin

---
+++ config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function maFieldDelegate($delegate, $compile) {
+  var directive = $delegate[0];
+  var link = directive.link;
+  directive.compile = function(){
+    return function(scope, element) {
+      const field = scope.field();
+      if (field.type() !== 'fieldset'){
+        link.apply(this, arguments);
+        return;
+      }
+
+      const template = require('./maFieldTemplate.html');
+      element.append(template);
+      scope.field = field;
+      scope.entity = scope.entity();
+      scope.form = scope.form();
+      scope.datastore = scope.datastore();
+      scope.type = field.type();
+      $compile(element.contents())(scope);
+    };
+  };
+
+  return $delegate;
+}
+maFieldDelegate.$inject = ['$delegate', '$compile'];
+
+function config(NgAdminConfigurationProvider, $provide) {
+  // override ma-field to add our necessary fieldset stuff
+  $provide.decorator('maFieldDirective', maFieldDelegate);
+}
+
+config.$inject = ['NgAdminConfigurationProvider', '$provide'];
+
+export default function(app) {
+  app.config(config);
+}
